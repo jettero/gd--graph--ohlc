a248c6ffff2a184dd8e25ba66818fc6bcfd33c6a
Paul Miller <jettero@cpan.org>
2009-04-26 18:14:26 -0400

    make this actually work

--- mixed_candle.pl
+++ mixed_candle.pl
@@ -1,3 +1,13 @@
+#!/usr/bin/perl -Iblib/lib
+
+use IPC::System::Simple qw(systemx);
+
+BEGIN {
+    my @mtime = map {(stat $_)[9]} qw(Makefile Makefile.PL);
+    systemx(qw(perl Makefile.PL)) if $mtime[0] != $mtime[1];
+    systemx(qw(make -f Makefile));
+}
+
 use strict;
 use warnings;
 use List::Util qw(min max);
